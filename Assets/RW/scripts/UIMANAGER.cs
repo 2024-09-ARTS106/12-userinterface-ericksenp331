1e707a9d2366dc8587803535585cb46425655281
ericksenp331 <G00285154@stu.raritanval.edu>
2024-11-14 22:13:21 -0500

    userinterface assignment

---
+++ Assets/RW/scripts/UIMANAGER.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.
+UnityEngine.SceneManagement;
+
+public class UIMANAGER : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("RocketMouse");  
+    }
+    void Update()
+    // Update is called once per frame
+    
+    {
+        
+    }
+}
